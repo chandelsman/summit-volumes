80ef2fab964afa6e36956b01c750ae2c4a2f3799
Corey Handelsman <chandelsman@gmail.com>
2023-01-27 08:21:25 -0700

    Updated script to identify unique records with PHI

--- src/phi-records.R
+++ src/phi-records.R
@@ -5,32 +5,20 @@ library(here)
 # Import all records that were exported from LigoLab
 cases <-
   readr::read_delim(
-    here("data", "all-cases-2014.09.14-2022.03.29.csv"),
+    here("data", "ytd-results_2023.01.26.csv"),
     delim = ",",
     col_types = cols(
       Accession = col_double(),
       `Result ID` = col_character(),
-      `Client ID` = col_character(),
-      `Client Name` = col_character(),
       `Patient Name` = col_character(),
-      `Patient ID` = col_character(),
-      `Date of Birth` = col_datetime(),
-      `Result Created Time` = col_datetime(),
-      `Report Released Time` = col_datetime(),
-      GUID = col_character(),
-      `CPT Code` = col_character(),
-      Quantity = col_double(),
-      `CPT Created Time` = col_datetime()
+      `Date of Birth` = col_datetime()
     ),
     na = "NULL"
   )
 
 # Remove non-identifiable records and count unique name & DOB combinations
-phi_recs <- cases %>% 
-  filter(`Patient Name` != "CONFIDENTIAL, PATIENT") %>% 
-  # select(`Patient Name`, `Date of Birth`) %>% 
+phi_unique <- 
+  cases %>% 
+  filter(`Patient Name` != "CONFIDENTIAL, PATIENT|, ",
+         !str_detect(`Patient Name`, "(?i)^zz")) %>% 
   count(`Patient Name`, `Date of Birth`)
-
-# Tally records: Number of patients to notify in case of catastrophic breach
-phi_count <- 
-n_distinct(phi_recs$`Patient Name`, phi_recs$`Date of Birth`)
